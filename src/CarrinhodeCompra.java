d557cda0eeb35fe24751d7acaf821993ed3a2735
gleisson <gleissonfreitas@hotmail.com>
2017-11-27 20:42:47 -0200

    //Terceira classe

---
+++ src/CarrinhodeCompra.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class CarrinhodeCompra {
+	private List<Produto> itens = new ArrayList<>();
+
+	public void adicionaProduto(Produto p){
+		itens.add(p);
+	}
+	
+	public int total(){
+		int total = 0;
+		for (Produto p:itens)
+			total += p.getValor();
+		return total;
+		}
+	
+}
